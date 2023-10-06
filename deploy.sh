122f9ae9290f598a82ec15abdd31f5991f1e5816
zelin <zelinjiang@cuhk.edu.hk>
2023-10-06 13:47:22 +0800

    hhhhh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-mkdir -p /home/admin/sop_probot_documents/pythonenv
-cd /home/admin/sop_probot_documents/pythonenv
+mkdir -p /home/ubuntu/sop_probot_documents/pythonenv
+cd /home/ubuntu/sop_probot_documents/pythonenv
 python3 -m venv env
 source env/bin/activate
-cd /home/admin/sop_probot_documents
+cd /home/ubuntu/sop_probot_documents
 pip install -r requirements.txt
 pip install -r docs/requirements.txt
 ACTION=$1
