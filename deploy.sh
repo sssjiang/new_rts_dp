896bfa2af5b727be3f70610bb5fe80b2ea51ee56
Yufeng Zhang <kinginsun@163.com>
2023-10-06 15:14:59 +0800

    Recreated_host_folder

--- deploy.sh
+++ deploy.sh
@@ -2,21 +2,32 @@
 
 set -e
 
+ACTION=$1
+
 cd /home/ubuntu/sop_probot_documents
 if [ ! -d 'venv' ];then
     python3 -m venv venv
 fi
 source venv/bin/activate
 cd /home/ubuntu/sop_probot_documents/app
-pip install -r docs/requirements.txt
-ACTION=$1
+
+if [ "$ACTION" == "restart" ];then
+    pip install -r docs/requirements.txt
+fi
+
+usage() {
+    echo "Usage: $PROG_NAME {start|stop|restart}"
+    exit 2
+}
+
 start() {   
     echo "Start..."
     mkdocs serve -a 0.0.0.0:8000 > output.log 2>&1 &
 }
 stop() {
-    echo "stop..."
+    echo "Stop..."
     pkill -f "mkdocs serve"
+    echo "Stop done"
 }
 
 restart() {
@@ -34,6 +45,9 @@ case "$ACTION" in
     restart)
         stop
         start
+    ;;
+    *)
+        usage
     ;;
 esac
 
